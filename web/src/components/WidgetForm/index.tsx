66660ec74c6959dee7b522e4905ba3c6510cace7
Jadson-Jose <jadsonjosesilva@gmail.com>
2022-05-03 23:46:42 -0300

    Cont day 2 part2

--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -6,6 +6,8 @@ import bugImageUrl from '../../img/bug.svg';
 import ideaImageUrl from '../../img/idea.svg';
 import thoughtImageUrl from '../../img/thought.svg';
 import { FeedbackTypeStep } from "./Steps/FeedbackTypesStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
+import { FeedbackSuccessStep } from "./Steps/FeebbackSuccessStep";
 
 export const feedbackTypes = {
     BUG: {
@@ -36,19 +38,34 @@ export type FeedbackType = keyof typeof feedbackTypes
 export function WidgetForm() {
 
     const [feedbackType, setFeeedBackType] = useState<FeedbackType | null>(null)
+    const [feedbackSend, setfeedbackSend] = useState(false);
+
+    function handleRestartFeedback() {
+        setfeedbackSend(false);
+        setFeeedBackType(null);
+    }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            <header>
-                <span className="text-xl leading-6">Deixe seu Feeedback</span>
-                <CloseButton />
-            </header>
-            {!feedbackType ? (
 
-                <FeedbackTypeStep onFeedbackTypeChanged={setFeeedBackType} />
-         
+            {feedbackSend ? (
+                <FeedbackSuccessStep onFeedbackRestartRequest={handleRestartFeedback} />
             ) : (
-                <p>Hello World</p>
+
+                <>
+                    {!feedbackType ? (
+
+                        <FeedbackTypeStep onFeedbackTypeChanged={setFeeedBackType} />
+
+                    ) : (
+                        <FeedbackContentStep
+                            feedbackType={feedbackType}
+                            onFeedbackRestartRequest={handleRestartFeedback}
+                            onFeedbackSend={() => setfeedbackSend(true)}
+                        />
+                    )}
+                </>
+
             )}
             <footer>
                 Feito com &#9829; por <a className="underline ring-offset-2" href="https://jadson-portifolio.netlify.app/" target="_blank" >Jadson Silva</a>
--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -3,11 +3,18 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        brand : {
-          500: '#8257e6',
+        brand: {
+          300: '#996DFF',
+          500: '#8257e6'
         }
+      },
+      borderRadius: {
+        md: '4px'
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('tailwind-scrollbar')
+  ],
 }
